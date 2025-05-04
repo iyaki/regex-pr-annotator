3aac7095d096476713b9592b6be6c7ee85edf848
iyaki <me@iyaki.ar>
2025-05-03 22:58:18 -0300

    Test file

---
+++ test/sample.js
@@ -0,0 +1,5 @@
+// sample.js - sample file to generate annotations
+function doSomething() {
+    console.log("Starting doSomething"); // matches console.log rule
+    // TODO: implement error handling     // matches TODO rule
+}
